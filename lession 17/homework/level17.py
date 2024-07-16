24a10bac4d46a35fba9ec52cce51fc3e8f0a52c6
biblaABCD <nikolozbiblaia0@gmail.com>
2024-07-16 23:00:13 +0400

    second commit

---
+++ lession 17/homework/level17.py
@@ -0,0 +1,21 @@
+#1
+for txt in range(30):
+    print('GOA IS THE BEST')
+#2
+for i in range(5, 150):
+    print(i)
+#3
+for i in range(2, 50, 4):
+    print(i)
+#4
+for i in range(10):
+    print(i, 'GOA')
+#5
+for i in range(0, 11, 1):
+    print(i)
+#6
+number = int(input('Enter number: '))
+guess_number = int(input('Guess the number: '))
+
+while number != guess_number:
+    guess_again_number = int(input('Guess number again: '))
