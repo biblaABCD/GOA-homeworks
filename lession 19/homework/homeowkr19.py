fdee7d54e18d8d1dbe70043589bd6dc19dac42b2
biblaABCD <nikolozbiblaia0@gmail.com>
2024-07-25 21:32:59 +0400

    second commit

---
+++ lession 19/homework/homeowkr19.py
@@ -0,0 +1,39 @@
+
+# # # Print numbers from 126 to 0 and decrease it by 1, 2, 3, use For loop
+
+# for i in range(126, 0, -1):
+#     print(i)
+
+# for i in range(126, 0, -2):
+#     print(i)
+
+# for i in range(126, 0, -3):
+#     print(i)
+
+# #Print sum of numbers from 1 to 47, use For loop
+
+# num = 0
+# for i in range(1, 47):
+#     num += i
+#     print(num)
+
+
+# num2 = 0
+# for i in range(1, 47):
+#     num2 *= i
+# print(num2)
+# x = 0
+# y = 101
+
+#Print number from 100 to 1 by using While loop
+x = 0
+y = 101
+while 100 > x:
+    x += 1
+    y -= 1
+    print(y)
+
+
+
+
+
