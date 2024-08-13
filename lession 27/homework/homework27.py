2faa9fc5e1c2f1fc62edc76664724d58b54e0da6
biblaABCD <nikolozbiblaia0@gmail.com>
2024-08-13 19:55:21 +0400

    second commit

---
+++ lession 27/homework/homework27.py
@@ -0,0 +1,7 @@
+list1 =[3, "salami", 2.0, True, 7<8, "3", 1, 12.959, False, "gamarjoba", 1121]
+list1[2] = 7
+list1[3] = False
+list1[8] = True
+list1[5] = "12"
+list1[6] = "gamarjoba"
+print(list1)
